6c7c15b2dc270084e22479093cd77029d1d9b601
xCmac <dsemac@gmail.com>
2014-07-06 16:10:15 -0500

    Refactor

--- Cod4StatTool/Cod4StatTool/Form1.cs
+++ Cod4StatTool/Cod4StatTool/Form1.cs
@@ -22,82 +22,82 @@ namespace Cod4StatTool
 
         #region
             //======ASSAULT======
-            int m16_marksman = 0xC819C38;
-            int m16_expert = 0xC819C3C;
+            int m16_marksman;
+            int m16_expert;
 
-            int ak47_marksman = 0xC819C40;
-            int ak47_expert = 0xC819C44;
+            int ak47_marksman;
+            int ak47_expert;
 
-            int g3_marksman = 0xC819C48;
-            int g3_expert = 0xC819C4C;
+            int g3_marksman;
+            int g3_expert;
 
-            int m4_marksman = 0xC819C50;
-            int m4_expert = 0xC819C54;
+            int m4_marksman;
+            int m4_expert;
 
-            int m14_marksman = 0xC819C58;
-            int m14_expert = 0xC819C5C;
+            int m14_marksman;
+            int m14_expert;
 
-            int g36c_marksman = 0xC819C60;
-            int g36c_expert = 0xC819C64;
+            int g36c_marksman;
+            int g36c_expert;
 
-            int mp44_expert = 0xC819C68;
+            int mp44_expert;
             //======ASSAULT======
 
 
             //======SMG======
-            int mp5_marksman = 0xC819C88;
-            int mp5_expert = 0xC819C8C;
+            int mp5_marksman;
+            int mp5_expert;
 
-            int skorpion_marksman = 0xC819C90;
-            int skorpion_expert = 0xC819C94;
+            int skorpion_marksman;
+            int skorpion_expert;
 
-            int miniuzi_marksman = 0xC819C98;
-            int miniuzi_expert = 0xC819C9C;
+            int miniuzi_marksman;
+            int miniuzi_expert;
 
-            int ak74u_marksman = 0xC819CA0;
-            int aK74u_expert = 0xC819CA4;
+            int ak74u_marksman;
+            int aK74u_expert;
 
-            int p90_marksman = 0xC819CA8;
-            int p90_expert = 0xC819CAC;
+            int p90_marksman;
+            int p90_expert;
             //======SMG======
 
 
             //======LMG======
-            int m249_marksman = 0xC819CD8;
-            int m249_expert = 0xC819CDC;
+            int m249_marksman;
+            int m249_expert;
 
-            int rpd_marksman = 0xC819CE0;
-            int rpd_expert = 0xC819CE4;
+            int rpd_marksman;
+            int rpd_expert;
 
-            int m60e4_marksman = 0xC819CE8;
-            int m60e4_expert = 0xC819CEC;
+            int m60e4_marksman;
+            int m60e4_expert;
             //======LMG======
 
 
             //======Shotgun======
-            int w1200_marksman = 0xC819D28;
-            int w1200_expert = 0xC819D2C;
+            int w1200_marksman;
+            int w1200_expert;
 
-            int m1014_marksman = 0xC819D30;
-            int m1014_expert = 0xC819D34;
+            int m1014_marksman;
+            int m1014_expert;
             //======Shotgun======
 
 
             //======Sniper======
-            int m40a3_marksman = 0xC819D80;
-            int m40a3_expert = 0xC819D84;
+            int m40a3_marksman;
+            int m40a3_expert;
 
-            int m21_marksman = 0xC819D78;
-            int m21_expert = 0xC819D7C;
+            int m21_marksman;
+            int m21_expert;
 
-            int dragunov_marksman = 0xC819D88;
-            int dragunov_expert = 0xC819D8C;
+            int dragunov_marksman;
+            int dragunov_expert;
 
-            int r700_marksman = 0xC819D90;
-            int r700_expert = 0xC819D94;
+            int r700_marksman;
+            int r700_expert;
 
-            int barrett_marksman = 0xC819D98;
-            int barret_expert = 0xC819D9C;
+            int barrett_marksman;
+            int barret_expert;
             //======Sniper======
         #endregion
         public Form1()
@@ -115,53 +115,53 @@ namespace Cod4StatTool
 
         private void ReadChallengeAddresses()
         {
-            Current_M16_M.Text = MemoryReader.ReadUInt(BaseAddress + m16_marksman).ToString();
-            Current_M16_E.Text = MemoryReader.ReadUInt(BaseAddress + m16_expert).ToString();
-            Current_AK47_M.Text = MemoryReader.ReadUInt(BaseAddress + ak47_marksman).ToString();
-            Current_AK47_E.Text = MemoryReader.ReadUInt(BaseAddress + ak47_expert).ToString();
-            Current_G3_M.Text = MemoryReader.ReadUInt(BaseAddress + g3_marksman).ToString();
-            Current_G3_E.Text = MemoryReader.ReadUInt(BaseAddress + g3_expert).ToString();
-            Current_M4_M.Text = MemoryReader.ReadUInt(BaseAddress + m4_marksman).ToString();
-            Current_M4_E.Text = MemoryReader.ReadUInt(BaseAddress + m4_expert).ToString();
-            Current_M14_M.Text = MemoryReader.ReadUInt(BaseAddress + m14_marksman).ToString();
-            Current_M14_E.Text = MemoryReader.ReadUInt(BaseAddress + m14_expert).ToString();
-            Current_G36C_M.Text = MemoryReader.ReadUInt(BaseAddress + g36c_marksman).ToString();
-            Current_G36C_E.Text = MemoryReader.ReadUInt(BaseAddress + g36c_expert).ToString();
-            Current_MP44_E.Text = MemoryReader.ReadUInt(BaseAddress + mp44_expert).ToString();
+            Current_M16_M.Text = MemoryReader.ReadUInt(m16_marksman).ToString();
+            Current_M16_E.Text = MemoryReader.ReadUInt(m16_expert).ToString();
+            Current_AK47_M.Text = MemoryReader.ReadUInt(ak47_marksman).ToString();
+            Current_AK47_E.Text = MemoryReader.ReadUInt(ak47_expert).ToString();
+            Current_G3_M.Text = MemoryReader.ReadUInt(g3_marksman).ToString();
+            Current_G3_E.Text = MemoryReader.ReadUInt(g3_expert).ToString();
+            Current_M4_M.Text = MemoryReader.ReadUInt(m4_marksman).ToString();
+            Current_M4_E.Text = MemoryReader.ReadUInt(m4_expert).ToString();
+            Current_M14_M.Text = MemoryReader.ReadUInt(m14_marksman).ToString();
+            Current_M14_E.Text = MemoryReader.ReadUInt(m14_expert).ToString();
+            Current_G36C_M.Text = MemoryReader.ReadUInt(g36c_marksman).ToString();
+            Current_G36C_E.Text = MemoryReader.ReadUInt(g36c_expert).ToString();
+            Current_MP44_E.Text = MemoryReader.ReadUInt(mp44_expert).ToString();
 
-            Current_MP5_M.Text = MemoryReader.ReadUInt(BaseAddress + mp5_marksman).ToString();
-            Current_MP5_E.Text = MemoryReader.ReadUInt(BaseAddress + mp5_expert).ToString();
-            Current_SKORP_M.Text = MemoryReader.ReadUInt(BaseAddress + skorpion_marksman).ToString();
-            Current_SKORP_E.Text = MemoryReader.ReadUInt(BaseAddress + skorpion_expert).ToString();
-            Current_UZI_M.Text = MemoryReader.ReadUInt(BaseAddress + miniuzi_marksman).ToString();
-            Current_UZI_E.Text = MemoryReader.ReadUInt(BaseAddress + miniuzi_expert).ToString();
-            Current_AK47U_M.Text = MemoryReader.ReadUInt(BaseAddress + ak74u_marksman).ToString();
-            Current_AK47U_E.Text = MemoryReader.ReadUInt(BaseAddress + aK74u_expert).ToString();
-            Current_P90_M.Text = MemoryReader.ReadUInt(BaseAddress + p90_marksman).ToString();
-            Current_P90_E.Text = MemoryReader.ReadUInt(BaseAddress + p90_expert).ToString();
+            Current_MP5_M.Text = MemoryReader.ReadUInt(mp5_marksman).ToString();
+            Current_MP5_E.Text = MemoryReader.ReadUInt(mp5_expert).ToString();
+            Current_SKORP_M.Text = MemoryReader.ReadUInt(skorpion_marksman).ToString();
+            Current_SKORP_E.Text = MemoryReader.ReadUInt(skorpion_expert).ToString();
+            Current_UZI_M.Text = MemoryReader.ReadUInt(miniuzi_marksman).ToString();
+            Current_UZI_E.Text = MemoryReader.ReadUInt(miniuzi_expert).ToString();
+            Current_AK47U_M.Text = MemoryReader.ReadUInt(ak74u_marksman).ToString();
+            Current_AK47U_E.Text = MemoryReader.ReadUInt(aK74u_expert).ToString();
+            Current_P90_M.Text = MemoryReader.ReadUInt(p90_marksman).ToString();
+            Current_P90_E.Text = MemoryReader.ReadUInt(p90_expert).ToString();
 
-            Current_M249_M.Text = MemoryReader.ReadUInt(BaseAddress + m249_marksman).ToString();
-            Current_M249_E.Text = MemoryReader.ReadUInt(BaseAddress + m249_expert).ToString();
-            Current_RPD_M.Text = MemoryReader.ReadUInt(BaseAddress + rpd_marksman).ToString();
-            Current_RPD_E.Text = MemoryReader.ReadUInt(BaseAddress + rpd_expert).ToString();
-            Current_M60E4_M.Text = MemoryReader.ReadUInt(BaseAddress + m60e4_marksman).ToString();
-            Current_M60E4_E.Text = MemoryReader.ReadUInt(BaseAddress + m60e4_expert).ToString();
+            Current_M249_M.Text = MemoryReader.ReadUInt(m249_marksman).ToString();
+            Current_M249_E.Text = MemoryReader.ReadUInt(m249_expert).ToString();
+            Current_RPD_M.Text = MemoryReader.ReadUInt(rpd_marksman).ToString();
+            Current_RPD_E.Text = MemoryReader.ReadUInt(rpd_expert).ToString();
+            Current_M60E4_M.Text = MemoryReader.ReadUInt(m60e4_marksman).ToString();
+            Current_M60E4_E.Text = MemoryReader.ReadUInt(m60e4_expert).ToString();
 
-            Current_W1200_M.Text = MemoryReader.ReadUInt(BaseAddress + w1200_marksman).ToString();
-            Current_W1200_E.Text = MemoryReader.ReadUInt(BaseAddress + w1200_expert).ToString();
-            Current_M1014_M.Text = MemoryReader.ReadUInt(BaseAddress + m1014_marksman).ToString();
-            Current_M1014_E.Text = MemoryReader.ReadUInt(BaseAddress + m1014_expert).ToString();
+            Current_W1200_M.Text = MemoryReader.ReadUInt(w1200_marksman).ToString();
+            Current_W1200_E.Text = MemoryReader.ReadUInt(w1200_expert).ToString();
+            Current_M1014_M.Text = MemoryReader.ReadUInt(m1014_marksman).ToString();
+            Current_M1014_E.Text = MemoryReader.ReadUInt(m1014_expert).ToString();
 
-            Current_M40A3_M.Text = MemoryReader.ReadUInt(BaseAddress + m40a3_marksman).ToString();
-            Current_M40A3_E.Text = MemoryReader.ReadUInt(BaseAddress + m40a3_expert).ToString();
-            Current_M21_M.Text = MemoryReader.ReadUInt(BaseAddress + m21_marksman).ToString();
-            Current_M21_E.Text = MemoryReader.ReadUInt(BaseAddress + m21_expert).ToString();
-            Current_DRAGUNOV_M.Text = MemoryReader.ReadUInt(BaseAddress + dragunov_marksman).ToString();
-            Current_DRAGUNOV_E.Text = MemoryReader.ReadUInt(BaseAddress + dragunov_expert).ToString();
-            Current_R700_M.Text = MemoryReader.ReadUInt(BaseAddress + r700_marksman).ToString();
-            Current_R700_E.Text = MemoryReader.ReadUInt(BaseAddress + r700_expert).ToString();
-            Current_BARRET_M.Text = MemoryReader.ReadUInt(BaseAddress + barrett_marksman).ToString();
-            Current_BARRET_E.Text = MemoryReader.ReadUInt(BaseAddress + barret_expert).ToString();
+            Current_M40A3_M.Text = MemoryReader.ReadUInt(m40a3_marksman).ToString();
+            Current_M40A3_E.Text = MemoryReader.ReadUInt(m40a3_expert).ToString();
+            Current_M21_M.Text = MemoryReader.ReadUInt(m21_marksman).ToString();
+            Current_M21_E.Text = MemoryReader.ReadUInt(m21_expert).ToString();
+            Current_DRAGUNOV_M.Text = MemoryReader.ReadUInt(dragunov_marksman).ToString();
+            Current_DRAGUNOV_E.Text = MemoryReader.ReadUInt(dragunov_expert).ToString();
+            Current_R700_M.Text = MemoryReader.ReadUInt(r700_marksman).ToString();
+            Current_R700_E.Text = MemoryReader.ReadUInt(r700_expert).ToString();
+            Current_BARRET_M.Text = MemoryReader.ReadUInt(barrett_marksman).ToString();
+            Current_BARRET_E.Text = MemoryReader.ReadUInt(barret_expert).ToString();
         }
 
         private void ReadMemory_Click(object sender, EventArgs e)
@@ -175,6 +175,8 @@ namespace Cod4StatTool
                 MemoryReader.OpenProcess();
 
                 gamefound = true;
+
+                SetUpChallengeAddresses();
             }
             catch (Exception exception)
             {
@@ -182,6 +184,53 @@ namespace Cod4StatTool
             }
 
             ReadMemoryButton.Enabled = false;
+        }
+
+        private void SetUpChallengeAddresses()
+        {
+            m16_marksman = BaseAddress + 0xC819C38;
+            m16_expert = BaseAddress + 0xC819C3C;
+            ak47_marksman = BaseAddress + 0xC819C40;
+            ak47_expert = BaseAddress + 0xC819C44;
+            g3_marksman = BaseAddress + 0xC819C48;
+            g3_expert = BaseAddress + 0xC819C4C;
+            m4_marksman = BaseAddress + 0xC819C50;
+            m4_expert = BaseAddress + 0xC819C54;
+            m14_marksman = BaseAddress + 0xC819C58;
+            m14_expert = BaseAddress + 0xC819C5C;
+            g36c_marksman = BaseAddress + 0xC819C60;
+            g36c_expert = BaseAddress + 0xC819C64;
+            mp44_expert = BaseAddress + 0xC819C68;
+            mp5_marksman = BaseAddress + 0xC819C88;
+            mp5_expert = BaseAddress + 0xC819C8C;
+            skorpion_marksman = BaseAddress + 0xC819C90;
+            skorpion_expert = BaseAddress + 0xC819C94;
+            miniuzi_marksman = BaseAddress + 0xC819C98;
+            miniuzi_expert = BaseAddress + 0xC819C9C;
+            ak74u_marksman = BaseAddress + 0xC819CA0;
+            aK74u_expert = BaseAddress + 0xC819CA4;
+            p90_marksman = BaseAddress + 0xC819CA8;
+            p90_expert = BaseAddress + 0xC819CAC;                        
+            m249_marksman = BaseAddress + 0xC819CD8;
+            m249_expert = BaseAddress + 0xC819CDC;
+            rpd_marksman = BaseAddress + 0xC819CE0;
+            rpd_expert = BaseAddress + 0xC819CE4;
+            m60e4_marksman = BaseAddress + 0xC819CE8;
+            m60e4_expert = BaseAddress + 0xC819CEC;            
+            w1200_marksman = BaseAddress + 0xC819D28;
+            w1200_expert = BaseAddress + 0xC819D2C;
+            m1014_marksman = BaseAddress + 0xC819D30;
+            m1014_expert = BaseAddress + 0xC819D34;
+            m40a3_marksman = BaseAddress + 0xC819D80;
+            m40a3_expert = BaseAddress + 0xC819D84;
+            m21_marksman = BaseAddress + 0xC819D78;
+            m21_expert = BaseAddress + 0xC819D7C;
+            dragunov_marksman = BaseAddress + 0xC819D88;
+            dragunov_expert = BaseAddress + 0xC819D8C;
+            r700_marksman = BaseAddress + 0xC819D90;
+            r700_expert = BaseAddress + 0xC819D94;
+            barrett_marksman = BaseAddress + 0xC819D98;
+            barret_expert = BaseAddress + 0xC819D9C;
         }
 
         private void ValidateAndWriteToMemory(int addressToWriteTo, String textBoxText, KeyEventArgs e)
